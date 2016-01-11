6e2432259e1adf77acbbd0deeffce669970d63da
Jan Ruusuvuori <jan.ruusuvuori@affecto.com>
2016-01-11 17:22:57 +0200

    Wrap Cached in WithCacheControl

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,10 +7,19 @@ import play.api.Play.current
 
 object Application extends Controller {
 
-  def index = Cached("index") {
+  case class WithCacheControl(action: EssentialAction) extends EssentialAction {
+    def apply(request: RequestHeader) = {
+      implicit val executionContext = play.api.libs.concurrent.Execution.defaultContext
+      action(request).map { result =>
+        result.withHeaders(("Cache-Control", "max-age=123"))
+      }
+    }
+  }
+
+  def index = WithCacheControl { Cached("index") {
     Action {
       Ok(views.html.index("Your new application is ready."))
     }
-  }
+  }}
 
 }
