790b9679e1f545e45fb363ddfb7a4580b0d6f7c3
Jan Ruusuvuori <jan.ruusuvuori@affecto.com>
2016-01-11 18:25:25 +0200

    Determine cache duration from Expires header set in innermost Action

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,13 @@
 package controllers
 
+import org.joda.time.{Duration, DateTime}
 import play.api.cache.Cached
 import play.api.mvc._
 
 import play.api.Play.current
 
 object Application extends Controller {
+  val dateFormat = play.api.http.dateFormat
 
   case class WithCacheControl(action: EssentialAction) extends EssentialAction {
     def apply(request: RequestHeader) = {
@@ -18,9 +20,20 @@ object Application extends Controller {
 
   val key: (RequestHeader) => String = { case rh => rh.uri }
 
-  def index = WithCacheControl { Cached(key, 5) {
+  val cacheDurationFromExpiresHeader: scala.PartialFunction[ResponseHeader, scala.concurrent.duration.Duration] = {
+    case rh => rh.headers.get(EXPIRES).map(dateFormat.parseDateTime) match {
+      case None => scala.concurrent.duration.Duration(5, scala.concurrent.duration.SECONDS)
+      case Some(expires) =>
+        val expiresIn = new Duration(DateTime.now(), expires).getStandardSeconds
+        scala.concurrent.duration.Duration(expiresIn, scala.concurrent.duration.SECONDS)
+    }
+  }
+
+  def index = WithCacheControl { Cached(key, cacheDurationFromExpiresHeader) {
     Action {
+      val expires = DateTime.now().plusSeconds(5).toString(dateFormat)
       Ok(views.html.index("Your new application is ready."))
+        .withHeaders((EXPIRES, expires))
     }
   }}
 
