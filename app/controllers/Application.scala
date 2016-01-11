359dfcded2e761df8aba52d92874a699d2af0683
Jan Ruusuvuori <jan.ruusuvuori@affecto.com>
2016-01-11 17:16:10 +0200

    Cached

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,16 @@
 package controllers
 
-import play.api._
+import play.api.cache.Cached
 import play.api.mvc._
+
+import play.api.Play.current
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index = Cached("index") {
+    Action {
+      Ok(views.html.index("Your new application is ready."))
+    }
   }
 
 }
